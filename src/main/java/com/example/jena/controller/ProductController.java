3c8776259c76e55020a7124a535e58b30de458ec
RBSaad <RBSaad@NB-RBS>
2023-10-31 21:31:11 +0100

    commit

--- src/main/java/com/example/jena/controller/ProductController.java
+++ src/main/java/com/example/jena/controller/ProductController.java
@@ -23,16 +23,15 @@ public class ProductController {
     public ResponseEntity<Object> sparQl() {
         String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
                 "PREFIX Projet-sem: <http://www.semanticweb.org/aminebarguellil/ontologies/2023/9/Projet-sem#>\n" +
-                "SELECT ?id ?name ?description ?price ?quantity ?weight ?image_url \n" +
+                "SELECT ?idProduct ?name ?description ?price ?quantity ?weight  \n" +
                 "WHERE {\n" +
                 "  ?produit rdf:type Projet-sem:Produit .\n" +
-                "  ?produit Projet-sem:id ?id .\n" +
+                "  ?produit Projet-sem:idProduct ?idProduct .\n" +
                 "  ?produit Projet-sem:name ?name .\n" +
                 "  ?produit Projet-sem:description ?description .\n" +
                 "  ?produit Projet-sem:price ?price .\n" +
                 "  ?produit Projet-sem:quantity ?quantity .\n" +
                 "  ?produit Projet-sem:weight ?weight .\n" +
-                "  ?produit Projet-sem:image_url ?image_url .\n"+
                 "}";
 
 
@@ -47,22 +46,71 @@ public class ProductController {
 
             while (results.hasNext()) {
                 QuerySolution solution = results.next();
-                RDFNode id = solution.get("id");
+                RDFNode idProduct = solution.get("idProduct");
                 RDFNode name = solution.get("name");
                 RDFNode description = solution.get("description");
                 RDFNode price = solution.get("price");
                 RDFNode quantity = solution.get("quantity");
                 RDFNode weight = solution.get("weight");
-                RDFNode image_url = solution.get("image_url");
 
                 Map<String, Object> resultItem = new HashMap<>();
-                resultItem.put("id", id.toString());
+                resultItem.put("idProduct", idProduct.toString());
                 resultItem.put("name", name.toString());
                 resultItem.put("description", description.toString());
                 resultItem.put("price", price.toString());
                 resultItem.put("quantity", quantity.toString());
                 resultItem.put("weight", weight.toString());
-                resultItem.put("image_url", image_url.toString());
+
+                queryResults.add(resultItem);
+            }
+
+            return new ResponseEntity<>(queryResults, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    @GetMapping("/productsByName")
+    public ResponseEntity<Object> getProductsByName(@RequestParam(value = "nameparam", required = false) String nameparam) {
+        String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX Projet-sem: <http://www.semanticweb.org/aminebarguellil/ontologies/2023/9/Projet-sem#>\n" +
+                "SELECT ?idProduct ?name ?description ?price ?quantity ?weight\n" +
+                "WHERE {\n" +
+                "  ?produit rdf:type Projet-sem:Produit .\n" +
+                "  ?produit Projet-sem:idProduct ?idProduct .\n" +
+                "  ?produit Projet-sem:name ?name .\n" +
+                "  ?produit Projet-sem:description ?description .\n" +
+                "  ?produit Projet-sem:price ?price .\n" +
+                "  ?produit Projet-sem:quantity ?quantity .\n" +
+                "  ?produit Projet-sem:weight ?weight .\n" +
+                "  FILTER (?name = \"" + nameparam + "\")\n" +
+                "}";
+
+
+        String serviceEndpoint = "http://localhost:3030/ds/sparql";
+
+        Query query = QueryFactory.create(queryString);
+        try (QueryExecution qexec = QueryExecutionFactory.sparqlService(serviceEndpoint, query)) {
+            ResultSet results = qexec.execSelect();
+
+            List<Object> queryResults = new ArrayList<>();
+
+            while (results.hasNext()) {
+                QuerySolution solution = results.next();
+                RDFNode idProduct = solution.get("idProduct");
+                RDFNode nameResult = solution.get("name");
+                RDFNode description = solution.get("description");
+                RDFNode price = solution.get("price");
+                RDFNode quantity = solution.get("quantity");
+                RDFNode weight = solution.get("weight");
+
+                Map<String, Object> resultItem = new HashMap<>();
+                resultItem.put("idProduct", idProduct.toString());
+                resultItem.put("name", nameResult.toString());
+                resultItem.put("description", description.toString());
+                resultItem.put("price", price.toString());
+                resultItem.put("quantity", quantity.toString());
+                resultItem.put("weight", weight.toString());
 
                 queryResults.add(resultItem);
             }
@@ -74,21 +122,22 @@ public class ProductController {
         }
     }
 
-    @GetMapping("/productsByName")
-    public ResponseEntity<Object> getProductsByName(@RequestParam(value = "name", required = false) String name) {
+    @GetMapping("/productsByPrice")
+    public ResponseEntity<Object> getProductsByPrice(
+            @RequestParam(value = "minPrice", required = false) Double minPrice
+    ) {
         String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
                 "PREFIX Projet-sem: <http://www.semanticweb.org/aminebarguellil/ontologies/2023/9/Projet-sem#>\n" +
-                "SELECT ?id ?name ?description ?price ?quantity ?weight ?image_url \n" +
+                "SELECT ?idProduct ?name ?description ?price ?quantity ?weight\n" +
                 "WHERE {\n" +
-                "  ?product rdf:type Projet-sem:Product .\n" +
-                "  ?product Projet-sem:id ?id .\n" +
-                "  ?product Projet-sem:name ?name .\n" +
-                "  ?product Projet-sem:description ?description .\n" +
-                "  ?product Projet-sem:price ?price .\n" +
-                "  ?product Projet-sem:quantity ?quantity .\n" +
-                "  ?product Projet-sem:weight ?weight .\n" +
-                "  ?product Projet-sem:image_url ?image_url .\n" +
-                (name != null && !name.isEmpty() ? "  FILTER regex(?name, '" + name + "', 'i').\n" : "") +
+                "  ?produit rdf:type Projet-sem:Produit .\n" +
+                "  ?produit Projet-sem:idProduct ?idProduct .\n" +
+                "  ?produit Projet-sem:name ?name .\n" +
+                "  ?produit Projet-sem:description ?description .\n" +
+                "  ?produit Projet-sem:price ?price .\n" +
+                "  ?produit Projet-sem:quantity ?quantity .\n" +
+                "  ?produit Projet-sem:weight ?weight .\n" +
+                (minPrice != null ? "  FILTER (?price > " + minPrice + ").\n" : "") +
                 "}";
 
         String serviceEndpoint = "http://localhost:3030/ds/sparql";
@@ -101,22 +150,20 @@ public class ProductController {
 
             while (results.hasNext()) {
                 QuerySolution solution = results.next();
-                RDFNode id = solution.get("id");
-                RDFNode productName = solution.get("name");
+                RDFNode idProduct = solution.get("idProduct");
+                RDFNode nameResult = solution.get("name");
                 RDFNode description = solution.get("description");
                 RDFNode price = solution.get("price");
                 RDFNode quantity = solution.get("quantity");
                 RDFNode weight = solution.get("weight");
-                RDFNode image_url = solution.get("image_url");
 
                 Map<String, Object> resultItem = new HashMap<>();
-                resultItem.put("id", id.toString());
-                resultItem.put("name", productName.toString());
+                resultItem.put("idProduct", idProduct.toString());
+                resultItem.put("name", nameResult.toString());
                 resultItem.put("description", description.toString());
                 resultItem.put("price", price.toString());
                 resultItem.put("quantity", quantity.toString());
                 resultItem.put("weight", weight.toString());
-                resultItem.put("image_url", image_url.toString());
 
                 queryResults.add(resultItem);
             }
@@ -128,5 +175,59 @@ public class ProductController {
         }
     }
 
+    @GetMapping("/productsByCategory")
+    public ResponseEntity<Object> getProductsByCategory(@RequestParam(value = "category", required = false) String category) {
+        String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "PREFIX Projet-sem: <http://www.semanticweb.org/aminebarguellil/ontologies/2023/9/Projet-sem#>\n" +
+                "SELECT ?idProduct ?name ?description ?price ?quantity ?weight\n" +
+                "WHERE {\n" +
+                "  ?produit rdf:type Projet-sem:Produit .\n" +
+                "  ?produit Projet-sem:idProduct ?idProduct .\n" +
+                "  ?produit Projet-sem:name ?name .\n" +
+                "  ?produit Projet-sem:description ?description .\n" +
+                "  ?produit Projet-sem:price ?price .\n" +
+                "  ?produit Projet-sem:quantity ?quantity .\n" +
+                "  ?produit Projet-sem:weight ?weight .\n";
+        if (category != null && !category.isEmpty()) {
+            queryString += "  ?produit Projet-sem:Possede ?categoryUri .\n";
+            queryString += "  ?categoryUri Projet-sem:nameCategorie '" + category + "'.\n";
+        }
+
+        queryString += "}";
+
+        String serviceEndpoint = "http://localhost:3030/ds/sparql";
+
+        Query query = QueryFactory.create(queryString);
+        try (QueryExecution qexec = QueryExecutionFactory.sparqlService(serviceEndpoint, query)) {
+            ResultSet results = qexec.execSelect();
+
+            List<Object> queryResults = new ArrayList<>();
+
+            while (results.hasNext()) {
+                QuerySolution solution = results.next();
+                RDFNode idProduct = solution.get("idProduct");
+                RDFNode nameResult = solution.get("name");
+                RDFNode description = solution.get("description");
+                RDFNode price = solution.get("price");
+                RDFNode quantity = solution.get("quantity");
+                RDFNode weight = solution.get("weight");
+
+                Map<String, Object> resultItem = new HashMap<>();
+                resultItem.put("idProduct", idProduct.toString());
+                resultItem.put("name", nameResult.toString());
+                resultItem.put("description", description.toString());
+                resultItem.put("price", price.toString());
+                resultItem.put("quantity", quantity.toString());
+                resultItem.put("weight", weight.toString());
+
+                queryResults.add(resultItem);
+            }
+
+            return new ResponseEntity<>(queryResults, HttpStatus.OK);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>("Error occurred", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 
 }
